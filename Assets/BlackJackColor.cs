364a12c333ead3c57ea6117636d19043125bdb1c
Rehsinup <ulric.punisher@gmail.com>
2025-03-27 09:54:24 +0100

    unity project

---
+++ Assets/BlackJackColor.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlackjackColor : MonoBehaviour
+{
+    public void OnButtonClick()
+    {
+
+        float randomValue = Random.Range(0f, 1f);
+        string colorResult;
+
+        if (randomValue < 0.45f)
+            colorResult = "Noir";
+        else if (randomValue < 0.90f)
+            colorResult = "Rouge";
+        else
+            colorResult = "Vert";
+
+        Debug.Log("Couleur obtenue : " + colorResult);
+    }
+}
